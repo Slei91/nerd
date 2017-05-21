ad8b5941a367a5f7838cb13bec67bd3f7bac1f5f
Slei91 <danilych91@rambler.ru>
2017-05-21 13:50:23 +0300

    остался адаптив магазина
    остался адаптив магазина

--- js/common.js
+++ js/common.js
@@ -6,3 +6,16 @@ $(document).ready(function() {
 	});
 
 });	
+
+function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 17,
+          scrollwheel: false,
+          center: {lat: 59.939222, lng: 30.321901}
+        });
+        var marker = new google.maps.Marker({
+          position: {lat: 59.938722, lng: 30.323801},
+          map: map,
+          icon: "img/map-marker.png"
+        });
+      }
